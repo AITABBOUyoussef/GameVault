5a060a5e79ee56dbf0e6010861926487ae4d6c4a
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-04-03 11:14:56 +0100

    Add search functionality in search.js; implement input listener to filter games by title

---
+++ search.js
@@ -0,0 +1,16 @@
+import { games } from "./data.js";
+import { afficherGames } from "./UI.js";
+
+const input = document.getElementById("input");
+
+ export function SearchInput(){
+input.addEventListener("input", () => {
+const value = input.value.toLowerCase();
+
+
+
+const result = games.filter(game =>
+   game.title.toLowerCase().includes(value)
+  ); afficherGames(result)
+});}
+
