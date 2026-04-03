db6fb5036ae5c7ee022fa296d87ad10425957956
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-04-03 12:36:35 +0100

    Implement ajouterPanier function to manage cart item quantities

--- ajouter-Panier.js
+++ ajouter-Panier.js
@@ -6,5 +6,12 @@ let panier = [];
 const btnAdd = document.getElementById("btnAdd");
 
 export function ajouterPanier(id_jeu){
-    
+    const jeuChoisi = games.find(jeu=> jeu.id===id_jeu);
+    const  jeuPanier = panier.find(e=>e.id===id_jeu)
+    if(jeuPanier){
+        jeuPanier.quantite+=1;
+        console.log(panier);
+    }else{
+        
+    }
 }
